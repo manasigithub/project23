d62d2e68607497d72a288af1985924cb292b5e36
manasigithub <manikumarmanasi16@gmail.com>
2022-10-06 12:41:43 +0530

    loops

---
+++ index.js
@@ -0,0 +1,25 @@
+//1 flowchart
+for(var i=0;i<5;i++)
+{
+    let i=prompt('enter the count')
+
+}
+document.write("Count is : "+i);
+
+
+
+//2 flowchart
+var count=0;
+var total =0;
+
+while(count<5)
+{
+    var marks=prompt('enter marks <br>');
+    total = total + parseInt(marks);
+    count++;
+    
+    
+}
+document.write("<br> Total marks is : "+total);
+
+
